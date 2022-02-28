fdc194cd9a737ed34a9ad1edc436099d6b5354f8
Oleg Zhidkov <gzhidkov@gmail.com>
2022-02-28 16:54:04 +0200

    still trying

---
+++ src/com/javatutorial/Main.java
@@ -0,0 +1,11 @@
+package com.javatutorial;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int age = 30; // [type] [identifier] [assigner] [initial value]
+        // in variables, it is camel notation
+        
+        System.out.println(age);
+    }
+}
---
+++ src/com/javatutorial/Types/ReferenceTypes.java
@@ -0,0 +1,24 @@
+package com.javatutorial.Types;
+
+import java.util.Date;
+
+public class ReferenceTypes {
+    public static void main(String[] args) {
+
+        // we use reference types to store complex objects
+       byte age = 30;
+       // for reference types we need to allocate memory
+       Date now = new Date();
+       // object is an instance of a class
+        System.out.println(now);
+    }
+}
+
+/***
+ * Primitive vs Reference Types
+ * object has a reference to the location of values
+ *
+ * - reference types are copied by their reference
+ * - primitive types are copied by their values
+ * that are independent of each other
+ */
